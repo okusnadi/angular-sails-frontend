17e3643c2fd8608783ab90917f9054dd53c98c69
Marek Sitko <masitko@gmail.com>
2016-05-03 22:43:46 +0000

    added custom form elements

--- src/app/admin/client.campaign.script/script-controllers.js
+++ src/app/admin/client.campaign.script/script-controllers.js
@@ -9,30 +9,54 @@
 
   var scriptPageController = function ($scope, _script, $builder, $validator) {
 
-    console.log(_script);
+    $builder.registerComponent('sampleInput', {
+        group: 'from html',
+        label: 'Sample',
+        description: 'From html template',
+        placeholder: 'placeholder',
+        required: false,
+        validationOptions: [
+          {
+            label: 'none',
+            rule: '/.*/'
+          }, {
+            label: 'number',
+            rule: '[number]'
+          }, {
+            label: 'email',
+            rule: '[email]'
+          }, {
+            label: 'url',
+            rule: '[url]'
+          }
+        ],
+        templateUrl: '/frontend/admin/client.campaign.script/templates/template.html',
+        popoverTemplateUrl: '/frontend/admin/client.campaign.script/templates/popoverTemplate.html'
+      });
+      
     var checkbox, textbox;
-    textbox = $builder.addFormObject('default', {
-      component: 'textInput',
-      label: 'Name',
-      description: 'Your name',
-      placeholder: 'Your name',
-      required: true,
-      editable: false
-    });
-    checkbox = $builder.addFormObject('default', {
-      component: 'checkbox',
-      label: 'Pets',
-      description: 'Do you have any pets?',
-      options: ['Dog', 'Cat']
-    });
-//    $builder.addFormObject('default', {
-//      component: 'sampleInput'
+//    textbox = $builder.addFormObject('default', {
+//      component: 'textInput',
+//      label: 'Name',
+//      description: 'Your name',
+//      placeholder: 'Your name',
+//      required: true,
+//      editable: false
 //    });
+//    checkbox = $builder.addFormObject('default', {
+//      component: 'checkbox',
+//      label: 'Pets',
+//      description: 'Do you have any pets?',
+//      options: ['Dog', 'Cat']
+//    });
+    $builder.addFormObject('default', {
+      component: 'sampleInput'
+    });
     $scope.form = $builder.forms['default'];
     $scope.input = [];
     $scope.defaultValue = {};
-    $scope.defaultValue[textbox.id] = 'default value';
-    $scope.defaultValue[checkbox.id] = [true, true];
+//    $scope.defaultValue[textbox.id] = 'default value';
+//    $scope.defaultValue[checkbox.id] = [true, true];
 
     return $scope.submit = function () {
       return $validator.validate($scope, 'default').success(function () {
