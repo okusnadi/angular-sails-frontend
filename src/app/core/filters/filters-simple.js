a60258a10fe5d6b6356e330cde00eb06c2b1a62f
Marek Sitko <masitko@gmail.com>
2016-05-11 18:46:08 +0000

    fixed node info card

--- src/app/core/filters/filters-simple.js
+++ src/app/core/filters/filters-simple.js
@@ -1,11 +1,23 @@
 // Simple filters to be used in the application
-(function() {
+(function () {
   'use strict';
 
   angular.module('frontend.core.filters')
-    .filter('$trustedHtml', function($sce) {
+    .filter('$trustedHtml', function ($sce) {
       return $sce.trustAsHtml;
-  })
-  ;
-  
+    })
+
+    .filter('isEmpty', function () {
+      var bar;
+      return function (obj) {
+        for (bar in obj) {
+          if (obj.hasOwnProperty(bar)) {
+            return false;
+          }
+        }
+        return true;
+      };
+    })
+
+    ;
 }());
